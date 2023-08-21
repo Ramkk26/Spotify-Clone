9922d802da371b183db1354ac12cce0891dde230
Ramkk26 <ramkumar@zethic.com>
2023-08-21 14:31:43 +0530

    json file created and fetched the data

--- my-app/src/components/Search/search.js
+++ my-app/src/components/Search/search.js
@@ -1,215 +1,39 @@
-import SearchImg from "../../images/search-img.jpg";
+import React, { useState, useEffect } from "react";
+
 import Header from "../Header/header";
 import Navbar from "../Navbar/navbar";
 import SignupBar from "../SignupBar/SignupBar";
 import "./Search.css";
+import JsonData from "../json/music.json"
+export default function Search() {
+  const [data, setData] = useState([]);
 
-export default function Search( ) {
+  useEffect(() => {
+ 
+    setData(JsonData);
+  }, []);
+
   return (
     <>
-    <Navbar hidden={true}/>
-
-    <div className="Header-tab">
-      <Header   />
-
-      <div class="card-details Search-image">
-        <div class="container-fluid">
-          <div class="row gap-5 p-4 flex-wrap card-content-main">
-            <div class="col-lg-2 col-sm-12  col-md-6   card-content content-1">
-              <a href="music.html">
-                <h3 class="cardtitle">Podcasts</h3>
-                <img src={SearchImg} alt="loading..."></img>
-              </a>
-            </div>
-            <div class="col-lg-2 col-sm-12  col-md-6 card-content content-2">
-              <a href="music.html">
-                <h3 class="cardtitle">Made For You</h3>
-                <img src={SearchImg} alt="loading..."></img>
-              </a>
-            </div>
-            <div class="col-lg-2 col-sm-12  col-md-6  card-content content-3">
-              <a href="music.html">
-                <h3 class="cardtitle">Romance</h3>
-                <img src={SearchImg} alt="loading..."></img>
-              </a>
-            </div>
-            <div class="col-lg-2 col-sm-12  col-md-6  card-content content-4">
-              <a href="music.html">
-                <h3 class="cardtitle">Jazz</h3>
-                <img src={SearchImg} alt="loading..."></img>
-              </a>
-            </div>
-            <div class="col-lg-2 col-sm-12  col-md-6  card-content content-5">
-              <a href="music.html">
-                <h3 class="cardtitle">Rock</h3>
-                <img src={SearchImg} alt="loading..."></img>
-              </a>
-            </div>
-            <div class="col-lg-2 col-sm-12  col-md-6  card-content content-6">
-              <a href="music.html"></a>
-              <h3 class="cardtitle">Charts</h3>
-              <img src={SearchImg} alt="loading..."></img>
-            </div>
-            <div class="col-lg-2 col-sm-12  col-md-6 card-content content-7">
-              <a href="music.html">
-                <h3 class="cardtitle">New Release</h3>
-                <img src={SearchImg} alt="loading..."></img>
-              </a>
-            </div>
-            <div class="col-lg-2 col-sm-12  col-md-6  card-content content-8">
-              <a href="music.html">
-                <h3 class="cardtitle">Tamil</h3>
-                <img src={SearchImg} alt="loading..."></img>
-              </a>
-            </div>
-            <div class="col-lg-2  col-sm-12  col-md-6  card-content content-9">
-              <a href="music.html">
-                <h3 class="cardtitle">Telugu</h3>
-                <img src={SearchImg} alt="loading..."></img>
-              </a>
-            </div>
-            <div class="col-lg-2 col-sm-12  col-md-6  card-content content-10">
-              <a href="music.html">
-                <h3 class="cardtitle">Malayalam</h3>
-                <img src={SearchImg} alt="loading..."></img>
-              </a>
-            </div>
-            <div class="col-lg-2 col-sm-12  col-md-6  card-content content-11">
-              <a href="music.html">
-                <h3 class="cardtitle">Hindi</h3>
-                <img src={SearchImg} alt="loading..."></img>
-              </a>
-            </div>
-            <div class="col-lg-2  col-sm-12  col-md-6  card-content content-12">
-              <a href="music.html">
-                <h3 class="cardtitle">Arabic</h3>
-                <img src={SearchImg} alt="loading..."></img>
-              </a>
-            </div>
-            <div class="col-lg-2 col-sm-12  col-md-6  card-content content-13">
-              <a href="music.html">
-                <h3 class="cardtitle">Englih</h3>
-                <img src={SearchImg} alt="loading..."></img>
-              </a>
-            </div>
-            <div class="col-lg-2  col-sm-12  col-md-6   card-content content-14">
-              <a href="music.html">
-                <h3 class="cardtitle">Kannada</h3>
-                <img src={SearchImg} alt="loading..."></img>
-              </a>
-            </div>
-            <div class="col-lg-2   col-sm-12  col-md-6   card-content content-15">
-              <a href="music.html">
-                <h3 class="cardtitle">Marathi</h3>
-                <img src={SearchImg} alt="loading..."></img>
-              </a>
-            </div>
-            <div class="col-lg-2   col-sm-12  col-md-6   card-content content-15">
-              <a href="music.html">
-                <h3 class="cardtitle">Marathi</h3>
-                <img src={SearchImg} alt="loading..."></img>
-              </a>
-            </div>
-            <div class="col-lg-2  col-sm-12  col-md-6   card-content content-1">
-              <a href="music.html">
-                <h3 class="cardtitle">Podcasts</h3>
-                <img src={SearchImg} alt="loading..."></img>
-              </a>
-            </div>
-            <div class="col-lg-2  col-sm-12  col-md-6   card-content content-2">
-              <a href="music.html">
-                <h3 class="cardtitle">Made For You</h3>
-                <img src={SearchImg} alt="loading..."></img>
-              </a>
-            </div>
-            <div class="col-lg-2 col-sm-12  col-md-6  card-content content-3">
-              <a href="music.html">
-                <h3 class="cardtitle">Romance</h3>
-                <img src={SearchImg} alt="loading..."></img>
-              </a>
-            </div>
-            <div class="col-lg-2  col-sm-12  col-md-6 card-content content-4">
-              <a href="music.html">
-                <h3 class="cardtitle">Jazz</h3>
-                <img src={SearchImg} alt="loading..."></img>
-              </a>
-            </div>
-            <div class="col-lg-2 col-sm-12  col-md-6  card-content content-5">
-              <a href="music.html">
-                <h3 class="cardtitle">Rock</h3>
-                <img src={SearchImg} alt="loading..."></img>
-              </a>
-            </div>
-            <div class="col-lg-2 col-sm-12  col-md-6  card-content content-6">
-              <a href="music.html"></a>
-              <h3 class="cardtitle">Charts</h3>
-              <img src={SearchImg} alt="loading..."></img>
-            </div>
-            <div class="col-lg-2 col-sm-12  col-md-6  card-content content-7">
-              <a href="music.html">
-                <h3 class="cardtitle">New Release</h3>
-                <img src={SearchImg} alt="loading..."></img>
-              </a>
-            </div>
-            <div class="col-lg-2 col-sm-12  col-md-6  card-content content-8">
-              <a href="music.html">
-                <h3 class="cardtitle">Tamil</h3>
-                <img src={SearchImg} alt="loading..."></img>
-              </a>
-            </div>
-            <div class="col-lg-2 col-sm-12  col-md-6  card-content content-9">
-              <a href="music.html">
-                <h3 class="cardtitle">Telugu</h3>
-                <img src={SearchImg} alt="loading..."></img>
-              </a>
-            </div>
-            <div class="col-lg-2 col-sm-12  col-md-6  card-content content-10">
-              <a href="music.html">
-                <h3 class="cardtitle">Malayalam</h3>
-                <img src={SearchImg} alt="loading..."></img>
-              </a>
-            </div>
-            <div class="col-lg-2 col-sm-12  col-md-6  card-content content-11">
-              <a href="music.html">
-                <h3 class="cardtitle">Hindi</h3>
-                <img src={SearchImg} alt="loading..."></img>
-              </a>
-            </div>
-            <div class="col-lg-2 col-sm-12  col-md-6  card-content content-12">
-              <a href="music.html">
-                <h3 class="cardtitle">Arabic</h3>
-                <img src={SearchImg} alt="loading..."></img>
-              </a>
-            </div>
-            <div class="col-lg-2 col-sm-12  col-md-6  card-content content-13">
-              <a href="music.html">
-                <h3 class="cardtitle">Englih</h3>
-                <img src={SearchImg} alt="loading..."></img>
-              </a>
-            </div>
-            <div class="col-lg-2  col-sm-12  col-md-6   card-content content-14">
-              <a href="music.html">
-                <h3 class="cardtitle">Kannada</h3>
-                <img src={SearchImg} alt="loading..."></img>
-              </a>
-            </div>
-            <div class="col-lg-2  col-sm-12  col-md-6    card-content content-15">
-              <a href="music.html">
-                <h3 class="cardtitle">Marathi</h3>
-                <img src={SearchImg} alt="loading..."></img>
-              </a>
-            </div>
-            <div class="col-lg-2  col-sm-12  col-md-6    card-content content-15">
-              <a href="music.html">
-                <h3 class="cardtitle">Marathi</h3>
-                <img src={SearchImg} alt="loading..."></img>
-              </a>
+      <Navbar hidden={true} />
+      <div className="Header-tab">
+        <Header />
+        <div className="card-details Search-image">
+          <div className="container-fluid">
+            <div className="row gap-5 p-4 flex-wrap card-content-main ">
+              {JsonData.map(item => (
+                <div key={item.id} className={`col-lg-2 col-sm-12 col-md-6 card-content ${item.id}  `} style={{ backgroundColor: item.background_color }} >
+                  <a href="music.html">
+                    <h3 className="cardtitle">{item.headLine}</h3>
+                    <img src={item.Url} alt="loading..." />
+                  </a>
+                </div>
+              ))}
             </div>
           </div>
         </div>
       </div>
-    </div>
-    <SignupBar/>
+      <SignupBar />
     </>
   );
 }
